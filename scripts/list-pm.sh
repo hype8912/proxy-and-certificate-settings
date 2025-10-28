3e616b2d9fb93e0f30b1b62178bee24c33137c72
e408268 <joshua.l.delong@lmco.com>
2025-10-28 09:51:23 -0400

    Updated package managers list

--- scripts/list-pm.sh
+++ scripts/list-pm.sh
@@ -14,26 +14,26 @@ pkmG="gel gem glasskube gradle guix"
 pkmH="harbourmaster hatch haxelib helm hex hpm huak"
 pkmI="iron"
 pkmJ="jetpack jlpkg"
-pkmK="kpm"
+pkmK="kiss kpm"
 pkmL="lein ligo lin lix luarocks lx"
 pkmM="magic mamba mason maximus microdnf micromamba mvn"
 pkmN="n ncl necro netpkg nimble ninite nitrile niv nix novus npm nuget nyssa"
 pkmO="opam openpkg openupm opkg oro"
-pkmP="pacaptr pacman paket parn pdm pear petget pip pipenv pipflow pipx pixi pkg pkgm pkgtool plz pnpm pod poet poetry port ppm pym pypm"
+pkmP="pacaptr pacman paket parn pdm pear petget pip pipenv pipflow pipx pixi pkg pkgm pkgtool plz pnpm pod poet poetry port portage ppm pyenv pym pypm"
 pkmQ="qp qpkg"
-pkmR="raco rpkg rpm rut rye rzget"
+pkmR="raco rpkg rpm rustup rut rye rzget"
 pkmS="sbt scarb scoop slackpkg slapt-get snap spack spago stack swift swupd"
-pkmT="teaport trex twine"
+pkmT="tdnf teaport trex twine"
 pkmU="unearth upm urpmi uv"
 pkmV="vanat vcpkg vite vpkg vlt volta"
 pkmW="winds winget wpkg"
 pkmX="xbps"
-pkmY="yarn yum"
+pkmY="yarn yast yum"
 pkmZ="zap zig zkg zpm zypper"
 
 # Combine all package managers
 package_managers="$pkmA $pkmB $pkmC $pkmD $pkmE $pkmF $pkmG $pkmH $pkmI $pkmJ $pkmK $pkmL $pkmM $pkmN $pkmO $pkmP $pkmQ $pkmR $pkmS $pkmT $pkmU $pkmV $pkmW $pkmX $pkmY $pkmZ"
-printf "Package managers- '%s'\n" "$package_managers"
+printf "Package managers- '%s'\n" "${package_managers}"
 printf "Checking for installed package managers...\n"
 
 # Check if printf supports -e flag, fallback to echo if not
@@ -44,14 +44,14 @@ else
 fi
 
 # Process each package manager
-for manager in $package_managers; do
-	if command -v "$manager" >/dev/null 2>&1; then
-		if [ "$use_printf" = 1 ]; then
-            printf "${green}%s is installed.${reset}\n" "$manager"
+for manager in ${package_managers}; do
+	if command -v "${manager}" >/dev/null 2>&1; then
+		if [ "${use_printf}" = 1 ]; then
+            printf "${green}%s is installed.${reset}\n" "${manager}"
         else
             echo "${manager} is installed."
         fi
 	else
-		printf "%s not found.\n" "$manager"
+		printf "%s not found.\n" "${manager}"
 	fi
 done
